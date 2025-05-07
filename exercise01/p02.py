41c2a43860000e5c29fdc11fdc5e78231b5c1848
root <juhyeong_kim@naver.com>
2025-05-07 07:16:40 +0000

    p01, p02

--- exercise01/p02.py
+++ exercise01/p02.py
@@ -1,9 +1,9 @@
-authrs = {
+authors = {
     "Charles Dickens": "1870",
     "William Thackeray": "1863",
     "Anthony Trollope": "1882",
     "Gerard Manley Hopkins": "1889"
+    }
 
-for author date in authors.items{}:
-    print "%s" % authors + " died in " + "%d." % Date
-}
+for author, date in authors.items():
+    print(author + " died in " + date)
